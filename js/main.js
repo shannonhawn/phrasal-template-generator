50fd64aa67127c542a192fc8e5fdd192a8c9517b
Shannon Hawn <shannonhawn@hotmail.com>
2016-02-26 17:30:42 -0500

    added the var for each prompt

---
+++ js/main.js
@@ -0,0 +1,11 @@
+alert('Lets make a story together!')
+
+var colour = prompt('A colour (orange, indigo, etc.)');
+
+var creature = prompt('A creature (dragon, moth, etc.)');
+
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like? a)Snacking b)Escaping c)Winning Enter the letter below:');
