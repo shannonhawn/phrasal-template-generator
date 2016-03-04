da987d603bc5328a56ec1695264d32e2df29f402
Shannon Hawn <shannonhawn@hotmail.com>
2016-03-03 20:20:03 -0500

    amended quotation marks

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-alert('Lets make a story together!');
+var name = alert('Lets make a story together!');
 
 var colour = prompt('A colour (orange, indigo, etc.)');
 
@@ -10,14 +10,14 @@ var verb = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like? a)Snacking b)Escaping c)Winning Enter the letter below:');
 
-if (story == 'a') {
+if (story = 'a') {
   document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + ' ' + verb + 'for hours.');
 }
 
-if (story == 'b') {
-  document.write('Ameilia' + verb + 'through the' + colour + 'adjective' + ' ' + colour + 'nebula escaping the space' + creature);
+if (story = 'b') {
+  document.write('Ameilia ' + verb + 'through the ' +  adjective + ' ' + colour + 'nebula escaping the space' + creature);
 }
 
-if (story == 'c') {
-  document.write('Jackson chose his' + adjective + ' ' + colour + ' ' + creature + 'card and ' + verb + 'it to the table knowing he won.');
+if (story = 'c') {
+  document.write('Jackson chose his' + adjective + ' , ' + colour + ' , ' + creature + 'card and ' + verb + 'it to the table knowing he won.');
 }
