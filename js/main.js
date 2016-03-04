2e700736b6d091c63e51d56f432598ac1589e1f5
Shannon Hawn <shannonhawn@hotmail.com>
2016-03-03 20:05:13 -0500

    amended spacing

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-var name = alert('Lets make a story together!');
+alert('Lets make a story together!');
+
 var colour = prompt('A colour (orange, indigo, etc.)');
+
 var creature = prompt('A creature (dragon, moth, etc.)');
+
 var adjective = prompt('An adjective (beautiful, super, etc.)');
+
 var verb = prompt('A past tense verb (ran, burped, etc.)');
+
 var story = prompt('Which story would you like? a)Snacking b)Escaping c)Winning Enter the letter below:');
 
 if (story == 'a') {
-  document.write ('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + ' ' + verb + 'for hours.');
+  document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + ' ' + verb + 'for hours.');
 }
 
 if (story == 'b') {
-  document.write ('Ameilia' + verb + 'through the' + colour + 'adjective' + ' ' + colour + 'nebula escaping the space' + creature);
+  document.write('Ameilia' + verb + 'through the' + colour + 'adjective' + ' ' + colour + 'nebula escaping the space' + creature);
 }
 
 if (story == 'c') {
-  document.write ('' + adjective + 'treats, the' + colour + 'bellied' + creature + ' ' + verb + 'for hours.');
+  document.write('Jackson chose his' + adjective + ' ' + colour + ' ' + creature + 'card and ' + verb + 'it to the table knowing he won.');
 }
