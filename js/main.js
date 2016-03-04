dfb44344609d19e971ab57652a2d63c160fc655b
Shannon Hawn <shannonhawn@hotmail.com>
2016-03-03 20:00:46 -0500

    added story options

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-alert('Lets make a story together!')
-
+var name = alert('Lets make a story together!');
 var colour = prompt('A colour (orange, indigo, etc.)');
-
 var creature = prompt('A creature (dragon, moth, etc.)');
-
 var adjective = prompt('An adjective (beautiful, super, etc.)');
-
 var verb = prompt('A past tense verb (ran, burped, etc.)');
-
 var story = prompt('Which story would you like? a)Snacking b)Escaping c)Winning Enter the letter below:');
+
+if (story == 'a') {
+  document.write ('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + ' ' + verb + 'for hours.');
+}
+
+if (story == 'b') {
+  document.write ('Ameilia' + verb + 'through the' + colour + 'adjective' + ' ' + colour + 'nebula escaping the space' + creature);
+}
+
+if (story == 'c') {
+  document.write ('' + adjective + 'treats, the' + colour + 'bellied' + creature + ' ' + verb + 'for hours.');
+}
